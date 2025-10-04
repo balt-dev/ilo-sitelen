afa9be1f47c2d9455a3c166cd6bd95c4e9c5f21f
balt-dev <heptor42+git@gmail.com>
2025-10-04 12:27:11 -0500

    Install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+cd /tmp
+if [ -d "ilo-sitelen" ];
+	rm -rf ilo-sitelen;
+fi;
+git clone git@github.com:balt-dev/ilo-sitelen.git
+cd ilo-sitelen
+cp -r table ~/.local/share/fcitx5
+cp -r inputmethod ~/.local/share/fcitx5
